1aa403bfb5413b8e19fa5dec57273d5a0431359a
Ricardo de Carvalho <ricardo.carvalho@yes.technology>
2025-11-13 16:38:53 -0300

    fix: Correct vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
-  root: 'example',
+  root: "example",
+  base: "/react-filter-group/",
   resolve: {
     alias: {
-      'react-filter-group': path.resolve(__dirname, './src'),
+      "react-filter-group": path.resolve(__dirname, "./src"),
     },
   },
 });
